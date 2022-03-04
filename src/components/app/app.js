6ccf67480002156f11ed8ee80be4c349d28012a8
Andrii <uzatsepin@gmail.com>
2022-03-04 21:35:42 +0200

    modified

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,9 +8,10 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 function App() {
 
     const data = [
-        {name: 'John C.', salary: 800, increase: true},
-        {name: 'Alex S.', salary: 650, increase: false},
-        {name: 'Andrii Z.', salary: 1200, increase: false}
+        {name: 'Vlad M.', salary: 300, increase: true, id: 1},
+        {name: 'John C.', salary: 800, increase: false, id: 2},
+        {name: 'Alex S.', salary: 650, increase: false, id: 3},
+        {name: 'Andrii Z.', salary: 1200, increase: false, id: 4}
     ]
 
     return (
